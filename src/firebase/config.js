34f7f5298eeeacf02b1dfd0afd2ac1afa17fd949
Oğuz Yıldız <36233491+oguzydz@users.noreply.github.com>
2020-05-24 12:36:49 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,4 @@
 export const config = {
-    
     apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
     databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
